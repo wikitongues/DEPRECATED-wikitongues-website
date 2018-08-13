acb4855db7ec3c7c9955bd6ecc41571ff5ff2fee
Robin Métral <robin@metral.ch>
2018-08-13 16:26:55 +0700

    Style alert
     - Flexbox
     - Adapt components/alert.php to flexbox styling
     - Flex items aligned left
     - Close button aligned right
     - Background color
     - FIX header overflow on x axis

--- components/alert.php
+++ components/alert.php
@@ -6,11 +6,11 @@
 	$alert_image
 	*/ ?>
 
-<div class="wt_alert">
-	<a href="<?php echo $alert_link ?>" target="_blank">
-		<div class="wt_alert-image" style="background:url('<?php echo $alert_image; ?>') center center no-repeat;"></div>
-		<h2><?php echo $alert_title; ?></h2>
-		<p><?php echo $alert_text; ?></p>
-	</a>
-	<button class="wt_close-module">Close</button>	
-</div>
+<aside class="wt_alert">
+    <img class="wt_alert-image" src="<?php echo $alert_image; ?>" alt="<?php echo $alert_title; ?>">
+    <article class="wt_alert-message">
+        <h2><?php echo $alert_title; ?></h2>
+        <?php echo $alert_text; ?>
+    </article>
+    <button class="wt_close-module">Close</button>
+</aside>
