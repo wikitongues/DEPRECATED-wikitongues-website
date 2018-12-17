e5699e5fef7c219deb3b60331356c8b8131a8491
Daniel Bogre Udell <daniel@wikitongues.org>
2018-12-16 19:09:39 -0500

    banner alert

--- components/alert.php
+++ components/alert.php
@@ -9,7 +9,7 @@
 <aside class="wt_alert wt_module">
     <img class="wt_alert-image" src="<?php echo $alert_image; ?>" alt="<?php echo $alert_title; ?>">
     <article class="wt_alert-message">
-        <h2><?php echo $alert_title; ?></h2>
+        <h2>New Video! <?php echo $alert_title; ?></h2>
         <p><?php echo $alert_text; ?></p>
     </article>
     <button class="wt_close-module">×</button>
