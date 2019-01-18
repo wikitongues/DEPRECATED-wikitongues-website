2c37fa2774e0b5b24864afb805b7db1c6dad1adb
Daniel Bogre Udell <udell.db@gmail.com>
2019-01-17 23:42:20 -0500

    steady progress

--- js/scripts.js
+++ js/scripts.js
@@ -18,9 +18,26 @@
 				$(this).closest('.wt_module').removeClass('slide-in');
 			});
 		}
+
+		// nav color change on scroll
+		function navColorChange() {
+			var $banner = $('.wt_banner, .wt_form-banner'),
+				$bannerOffset = $banner.position().top + $banner.outerHeight(true);
+
+			$(window).scroll(function(){
+				if ( $(window).scrollTop() > $bannerOffset ) { 
+					$('.header').addClass('background');
+				} else {
+					$('.header').removeClass('background');
+				}
+			});
+		}
 		
 		// load close module function
 		closeModule();
+
+		// load nav color change function
+		navColorChange();
 	});
 	
 })(jQuery, this);
