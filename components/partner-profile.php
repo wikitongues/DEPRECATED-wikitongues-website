0ff1d72ba27e27cbb4b8289cb19b53ffb2686073
Scott Rohrer <smrohrer@alumni.cmu.edu>
2019-02-08 14:18:26 -0500

    Added expandable partner profiles to Partners template

---
+++ components/partner-profile.php
@@ -0,0 +1,22 @@
+<?php
+/* The following variables may be defined for this template:
+    $i (required)
+    $profile_style (required)
+    $partner_name (required)
+	$partner_logo (required)
+	$partner_website (required)
+	$partner_bio (required)
+	*/ ?>
+
+<div class="wt_partner-profile" id="partner-profile-<?php echo($i); ?>" style="display: none; <?php echo($profile_style) ?>">
+    <div class="flex-container">
+        <div class="wt_partner-profile-image">
+            <img src="<?php echo($partner_logo['url']) ?>">
+        </div>
+        <div>
+            <h2><?php echo($partner_name) ?></h2>
+            <a href="<?php echo($partner_website); ?>"><?php echo($partner_website) ?></a>
+            <p><?php echo($partner_bio); ?></p>
+        </div>
+    </div>
+</div>
--- template-partners.php
+++ template-partners.php
@@ -16,17 +16,37 @@
 
 		echo '<ul id="wt_full-partners-list">';
 
+		$i = 0;
+
 		while ( $partners->have_posts() ) {
 			// initialize post data
 			$partners->the_post();
+
+			$partner_name = get_the_title();
 
 			// define content variables 
 			$partner_logo = get_field('partner_logo');
 			$partner_website = get_field('partner_website');
 			$partner_bio = get_field('partner_bio');
 
-			echo '<li class="wt_partner"><img src="'.$partner_logo['url'].'"></li>';
+			// assign to row of css grid
+			$grid_row = intdiv($i, 4) * 2 + 1;
 
+			// js function to call onclick
+			$function_call = 'expandPartner('.$i.')';
+
+			// css grid-row of image
+			$style = 'grid-row: '.$grid_row.'/'.($grid_row + 1);
+
+			// css grid-row of expandable profile
+			$profile_style = 'grid-row: '.($grid_row + 1).'/'.($grid_row + 2);
+
+			echo '<li class="wt_partner" id="partner-'.$i.'" onclick="'.$function_call.'" style="'.$style.'"><img src="'.$partner_logo['url'].'"></li>';
+
+			// expandable profile component
+			include( locate_template('components/partner-profile.php') );
+
+			$i++;
 		} 
 
 		wp_reset_postdata();
@@ -35,3 +55,15 @@
 	}
 
 get_footer(); ?>
+
+<script>
+function expandPartner (i) {
+	// collapse all other profiles
+	const allProfiles = document.querySelectorAll('.wt_partner-profile');
+	allProfiles.forEach(profile => profile.style.display = 'none');
+
+	// expand selected profile
+	const profile = document.getElementById(`partner-profile-${i}`);
+	profile.style.display = 'block';
+}
+</script>
