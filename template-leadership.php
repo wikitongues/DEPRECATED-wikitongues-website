929d0060ab9fbec4b70c1e0674d4e47905fed97c
Daniel Bogre Udell <daniel@wikitongues.org>
2019-08-29 12:49:07 -0400

    removed associate board

--- template-leadership.php
+++ template-leadership.php
@@ -82,38 +82,4 @@
 
 </div><!-- /two column face grid -->
 
-<h1>Associate Board</h1>
-
-<div class="wt_face-grid small">
-
-<?php 
-	$associates = get_field('associates');
-
-	// start the loop
-	if ( $associates ) {
-		
-		foreach ( $associates as $post ) { 
-			// initialize row data
-			setup_postdata($post);
-
-			// define content variables
-			$name = get_the_title($post);
-			$profile_picture = get_field('profile_picture');
-			$credentials = get_field('credentials');
-			$credentials_title = $credentials['title'];
-			$credentials_institution = $credentials['institution_or_company'];
-			$location = get_field('location');
-			$location_city = $location['city_and_territory'];
-			$location_country = $location['country'];
-			$bio = get_field('bio');
-
-			// load member profile template
-			include( locate_template('components/member-profile.php') );
-		}
-
-		wp_reset_postdata();
-	}  ?>
-
-</div><!-- /two column face grid -->
-
 <?php get_footer(); ?>
