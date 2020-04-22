1f82ba326652a8661c8ebebb6aa0883896b9984a
Scott Rohrer <smrohrer@alumni.cmu.edu>
2020-04-22 19:52:53 -0400

    Search form styling

--- searchform.php
+++ searchform.php
@@ -17,9 +17,9 @@ $wt_search_unique_id = uniqid('search-form-');
 
 $wt_search_aria_label = ! empty( $args['label'] ) ? 'aria-label="' . esc_attr( $args['label'] ) . '"' : '';
 ?>
-<form role="search" <?php echo $wt_search_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/videos' ) ); ?>">
+<form class="wt_search-form" role="search" <?php echo $wt_search_aria_label; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Escaped above. ?> method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="<?php echo esc_attr( $wt_search_unique_id ); ?>">
 		<input type="search" id="<?php echo esc_attr( $wt_search_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Wikitongues &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+	<input type="submit" class="search-submit wt_search-form__submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
 </form>
